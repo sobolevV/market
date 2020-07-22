1e45b8bced4a40993e1287a592a754d8f01f52b5
Vadim Sobolev <callofdutz@gmail.com>
2020-07-23 03:21:01 +0400

    removed ignore files

---
+++ mail_config.py
@@ -0,0 +1,8 @@
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+
+MAIL_USERNAME = "bsmchk20@gmail.com"
+MAIL_PASSWORD = "marketpassword1"
+MAIL_DEFAULT_SENDER = "bsmchk20@gmail.com"
